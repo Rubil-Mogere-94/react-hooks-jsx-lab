f636051a24c9343508c2a31e3333ac5f9e10c609
Rubil <mogererubil@gmail.com>
2025-08-01 09:23:57 +0300

    new commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { image } from "../data/data";
+// src/components/About.js
+import React from 'react';
+import { image } from '../data/data';
 
-function About() {
-  return <div>About</div>;
-}
+const About = () => {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Any content for the paragraph</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+};
 
 export default About;
